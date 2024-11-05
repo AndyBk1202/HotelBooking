edf794672fb3cbe0181a87c8bcf1654c0279cb5c
Andy <andybk.nguyen1202@gmail.com>
2024-11-05 21:55:24 +0700

    add note for review and promotion service

--- src/main/java/com/aplusplus/HotelBooking/service/interf/IPromotionService.java
+++ src/main/java/com/aplusplus/HotelBooking/service/interf/IPromotionService.java
@@ -4,11 +4,11 @@ import com.aplusplus.HotelBooking.dto.PromotionDTO;
 import com.aplusplus.HotelBooking.dto.Response;
 
 public interface IPromotionService {
-    Response createPromotion(PromotionDTO promotion);
-    Response getPromotionById(String promotionId);
-    Response getAllPromotion();
-    Response updatePromotion(PromotionDTO promotion);
+    Response createPromotion(PromotionDTO promotion); // thêm list các roomId
+    Response getPromotionById(String promotionId); // Admin xem chi tiết promotion
+    Response getAllPromotion(); // Admin
+    Response updatePromotion(PromotionDTO promotion); // Admin
     Response deletePromotion(String promotionId);
-
-    Response applyPromotionToRoom(String promotionId, String roomId);
+    Response applyPromotionToRoom(String promotionId, String roomId); // Liên quan Booking
+    Response getPromotionByRoomId(String roomId); // List các promotion áp dụng cho 1 room
 }
