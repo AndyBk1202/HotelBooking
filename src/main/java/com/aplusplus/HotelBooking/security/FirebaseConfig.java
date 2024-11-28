d3022aba0477388799fc10645a453a9db330575d
Andy <andybk.nguyen1202@gmail.com>
2024-11-28 14:49:43 +0700

    fix firebase config

--- src/main/java/com/aplusplus/HotelBooking/security/FirebaseConfig.java
+++ src/main/java/com/aplusplus/HotelBooking/security/FirebaseConfig.java
@@ -5,19 +5,20 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/serviceAccountKey.json");
+        InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
+        FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("hotelbooking-fc85a.appspot.com")
                 .build();
