e8eaf62e61b61bb99a684170b2a91630fde27404
danh55162 <43344870+danh23072004@users.noreply.github.com>
2024-11-28 15:12:31 +0700

    update promotions

--- src/main/java/com/aplusplus/HotelBooking/controller/PromotionController.java
+++ src/main/java/com/aplusplus/HotelBooking/controller/PromotionController.java
@@ -48,7 +48,7 @@ public class PromotionController {
     // user service updatePromotion
     // still has bugs, when removing room type from promotion list room type, it still remains in the list
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PutMapping("/update-promotion/{id}")
+    @PostMapping("/update-promotion/{id}")
     public ResponseEntity<Response> updatePromotion(
             @RequestPart(value = "promotion") PromotionDTO promotion,
             @RequestPart(value = "imageFile", required = false) MultipartFile imageFile,
